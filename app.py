9b82e32d72615c52ea7130cbfe64ceb17c833645
ZPXD <98432112+ZPXD@users.noreply.github.com>
2022-04-02 13:01:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+
+
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import (
+	StringField,
+	TextAreaField,
+	SubmitField,
+	PasswordField,
+	DateField,
+	SelectField
+)
 
 app=Flask(__name__)
 
+app.secret_key = 'super secret key'
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+	return render_template("index.html")
+
+
+# Logowanie
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
@@ -13,11 +29,28 @@ def login():
 
 @app.route('/signup', methods=["GET", "POST"])
 def signup():
-	return 'login'
+	form = SignupForm()
+
+	if form.validate_on_submit():
+		user = form.user.data
+		password = form.password.data
+		email = form.email.data
+	
+	return render_template("signup.html", form=form)
 
 @app.route('/logout', methods=["GET", "POST"])
 def logout():
-	return 'login'
+	return 'logout'
+
+
+@app.route('/signup_success', methods=["GET", "POST"])
+def signup_success():
+	return render_template("signup_success.html")
+
+
+
+
+
 
 @app.route('/ask_question', methods=["GET", "POST"])
 def question():
@@ -27,18 +60,33 @@ def question():
 def answer():
 	return "answer"
 
-@app.route('/list_of_questions', methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
 def list_of_questions():
 	return "list of questions"
 
-@app.route('/random_answer', methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
 def show_random_answer():
 	return "random answer"
 
-@app.route('/list_of_answers', methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
 def show_list_of_answers():
 	return "list of answers"
 
 
+
+class SignupForm(FlaskForm):
+	user = StringField('Login')
+	password = PasswordField('Password')
+	confirm_password = PasswordField('Repeat Password')
+	email = StringField('Email')
+	#recaptcha = RecaptchaField()
+	submit = SubmitField('Submit')
+
+	#password = PasswordField('New Password', [
+	#        validators.DataRequired(),
+	#        validators.EqualTo('confirm', message='Passwords must match')
+	#])
+
+
 if __name__=="__main__":
-    app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0')
