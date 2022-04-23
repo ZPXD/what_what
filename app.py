e8414d784b78f9b7496e82055d1f75b6e67198e8
ZPXD <zajeciaprogramowaniaxd@gmail.com>
2022-04-23 16:56:01 +0200

    DB and form for User working.

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 from flask_wtf import FlaskForm
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, TextAreaField, SubmitField, PasswordField, DateField, SelectField
 from wtforms.validators import DataRequired
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/xd.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.secret_key = ':)'
 
+db = SQLAlchemy(app)
 
 # Main
 
@@ -41,26 +48,68 @@ def index():
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
-	return 'login'
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if user.check_password(password):
+                return redirect( url_for('index'))
+        else:
+            return "user not found"
+    return render_template("login.html", form=form)
 
 @app.route('/signup', methods=["GET", "POST"])
 def signup():
-	form = SignupForm()
+    form = SignupForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        password = form.password.data
+        confirm_password = form.confirm_password.data
 
-	if form.validate_on_submit():
-		user = form.user.data
-		password = form.password.data
-		email = form.email.data
-	
-	return render_template("signup.html", form=form)
+        if User.query.filter_by(email=email).first():
+            return 'taki email już istnieje'
 
-@app.route('/logout', methods=["GET", "POST"])
+        user = User(name=name, email=email, password=password)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect( url_for('index'))
+    return render_template("signup.html", form=form)
+
+@app.route('/logout')
 def logout():
-	return 'logout'
+    return render_template("logout.html")
 
 @app.route('/signup_success', methods=["GET", "POST"])
 def signup_success():
 	return render_template("signup_success.html")
+
+
+# DB Models
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), unique=True, nullable=False)
+
+    def set_password(self,password):
+        self.password = generate_password_hash(password)
+     
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
+
+@app.before_first_request
+def create_all():
+    db.create_all()
 
 
 # Forms
@@ -74,7 +123,7 @@ class SignupForm(FlaskForm):
     name = StringField('nazwa użytkownika', validators=[DataRequired()])
     email = StringField('email', validators=[DataRequired()])
     password = StringField('hasło', validators=[DataRequired()])
-    password2 = StringField('powtórz hasło', validators=[DataRequired()])
+    confirm_password = StringField('powtórz hasło', validators=[DataRequired()])
     submit = SubmitField('załóż konto')
 
 
